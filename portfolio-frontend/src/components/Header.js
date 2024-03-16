56bf9c03334f2d0222d829d8aa148cb18fe53610
Kosy <okekekosy12@gmail.com>
2024-03-16 15:22:13 +0100

    fixed header bug

--- portfolio-frontend/src/components/Header.js
+++ portfolio-frontend/src/components/Header.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Container, Nav, Navbar, Offcanvas} from 'react-bootstrap'
 import './Header.css';
 import logo from '../assets/kosyLogo3.png'
 
 function Header() {
     // const [linkColor, setLinkColor] = useState('dark');
     const [navbarBg, setNavbarBg] = useState('transparent');
-    const [isMobile, setIsMobile] = useState('')
+    // const [isMobile, setIsMobile] = useState('')
     const [margin, setMargin] = useState('20px');
 
     const handleScroll = () => {
@@ -24,12 +24,8 @@ function Header() {
         setMargin('20px');
         
         }
+
     };
-
-    const mobileView = () => {
-        const checkIsMobile = window.innerWidth;
-
-    }
 
     useEffect(() =>{
         window.addEventListener('scroll', handleScroll)
@@ -38,30 +34,39 @@ function Header() {
         )
     })
   return (
+    
     <div style={{width: '100%',}}>
-        <Navbar collapseOnSelect expand="lg" fixed='top'  bg={navbarBg}  variant='dark' className="" style={{ backgroundColor: navbarBg,  }}>
+        <Navbar collapseOnSelect expand="lg" fixed='top'  bg={navbarBg}  variant='dark' className="mb-3" style={{   }}>
             <Container>
                 <Navbar.Brand href="/">
                     <img src={logo} alt='Kosy Logo' width='120px'/>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                <Nav className="me-auto">
-                    <Nav.Link href="#About">About</Nav.Link>
-                    <Nav.Link href="#Services">Services</Nav.Link>
-                    <Nav.Link href="#Skills">Skills</Nav.Link>
-                    <Nav.Link href="#Projects">Projects</Nav.Link>
-                    <Nav.Link href="#Cert">Certifications</Nav.Link>
-                    <Nav.Link href="#Contact">Contact</Nav.Link>
-                </Nav>
-                <Nav>
-                    <Nav.Link href="https://github.com/TheNexusDeveloper" target="_blank" rel="noopener"><i class="bi bi-github"></i></Nav.Link>
-                    <Nav.Link href="https://www.linkedin.com/in/kosisochukwu-okeke-505490242/" target="_blank" rel="noopener"><i class="bi bi-linkedin"></i></Nav.Link>
-                    <Nav.Link href="https://twitter.com/iamkosy11" target="_blank" rel="noopener"><i class="bi bi-twitter-x"></i></Nav.Link>
-                    <Nav.Link href="https://www.instagram.com/iamkosy11/" target="_blank" rel="noopener"><i class="bi bi-instagram"></i></Nav.Link>
-                    
-                </Nav>
-                </Navbar.Collapse>
+                <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                <Navbar.Offcanvas id="offcanvasNavbar-expand" placement="start"  aria-labelledby='offcanvasNavbar-expand' data-bs-theme="dark">
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id='offcanvasNavbarLabel-expand'>
+                            <img src={logo} alt='Kosy Logo' width='150px'/>
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="px-3 me-auto">
+                            <Nav.Link href="#About">About</Nav.Link>
+                            <Nav.Link href="#Services">Services</Nav.Link>
+                            <Nav.Link href="#Skills">Skills</Nav.Link>
+                            <Nav.Link href="#Projects">Projects</Nav.Link>
+                            <Nav.Link href="#Cert">Certifications</Nav.Link>
+                            <Nav.Link href="#Contact">Contact</Nav.Link>
+                        </Nav>
+                        <Nav className='px-3'>
+                            <Nav.Link href="https://github.com/TheNexusDeveloper" target="_blank" rel="noopener"><i class="bi bi-github"></i></Nav.Link>
+                            <Nav.Link href="https://www.linkedin.com/in/kosisochukwu-okeke-505490242/" target="_blank" rel="noopener"><i class="bi bi-linkedin"></i></Nav.Link>
+                            <Nav.Link href="https://twitter.com/iamkosy11" target="_blank" rel="noopener"><i class="bi bi-twitter-x"></i></Nav.Link>
+                            <Nav.Link href="https://www.instagram.com/iamkosy11/" target="_blank" rel="noopener"><i class="bi bi-instagram"></i></Nav.Link>
+                            
+                        </Nav>
+                    </Offcanvas.Body>
+                
+                </Navbar.Offcanvas>
             </Container>
         </Navbar>
 
