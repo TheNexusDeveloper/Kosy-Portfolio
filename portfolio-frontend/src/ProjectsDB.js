3950d617cddb92d7fd2e78d4e9f3b448f1cddba6
Kosy <okekekosy12@gmail.com>
2024-04-18 00:00:59 +0100

    update project stream

--- portfolio-frontend/src/ProjectsDB.js
+++ portfolio-frontend/src/ProjectsDB.js
@@ -1,6 +1,6 @@
 import MSA from './assets/Screenshot 2.png'
 import shop from './assets/Screenshot.png'
-import img1 from './assets/Screenshot (15).png'
+import img1 from './assets/Screenshot (61).png'
 import img2 from './assets/Screenshot (31).png'
 import img3 from './assets/IMG_3750.JPG'
 
@@ -14,7 +14,7 @@ const ProjectDB = [
         'stack': 'React, django', 
         'short': 'Developing Fullstack Web Applications for individuals, SMEs and organisations.',
         'desc': 'Developing Fullstack Web Applications for individuals, SMEs and organisations. Leveraging the power of the world wide web to create an online presence for Businesses and individuals.',
-        'repo': 'github',
+        'repo': 'https://github.com/TheNexusDeveloper',
         'demo': 'demo',
         
     }, 
@@ -27,7 +27,7 @@ const ProjectDB = [
         'stack': 'React, django',
         'short': 'Developing Fullstack Web Applications for individuals, SMEs and organisations.',
         'desc': '',
-        'repo': '',
+        'repo': 'https://github.com/TheNexusDeveloper',
         'demo': '',
        
     }, 
@@ -40,7 +40,7 @@ const ProjectDB = [
         'stack': 'React, django',
         'short': 'Developing Fullstack Web Applications for individuals, SMEs and organisations.',
         'desc': '',
-        'repo': '',
+        'repo': 'https://github.com/TheNexusDeveloper',
         'demo': '',
        
     }, 
@@ -53,8 +53,8 @@ const ProjectDB = [
         'stack': 'React, django',
         'short': 'Fullstack Mobile Applications for individuals, SMEs and organisations.',
         'desc': '',
-        'repo': '',
-        'demo': '',
+        'repo': 'https://github.com/TheNexusDeveloper',
+        'demo': 'https://www.figma.com/proto/B5Ej9JkASb6i5HAwImd2FA/Untitled?type=design&node-id=2-24&t=R5kmEMBEwEo3SRGd-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=35%3A26&mode=design',
        
     }, 
 
@@ -66,7 +66,7 @@ const ProjectDB = [
         'stack': 'CISCO',
         'short': 'Submarine cable map showing all active fiber optic cables across the globe.',
         'desc': 'The Submarine Cable Map is a free and regularly updated resource from TeleGeography. An interactive submarine cable map of the Internet fiber-optic submarine cable systems and cable landing stations. Fiber optic submarine cable systems, also known as undersea cables or subsea cables, are an essential part of the global Internet infrastructure.',
-        'repo': 'https://www.submarinecablemap.com/',
+        'repo': 'https://github.com/TheNexusDeveloper',
         'demo': 'https://www.submarinecablemap.com/',
        
     }, 
